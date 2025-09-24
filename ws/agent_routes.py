787893a86d4258770bc248f97ad59064929ec1fe
Dorian Cortes <19631146+doriandres@users.noreply.github.com>
2025-09-23 22:37:26 -0600

    fix: Enhance WebSocket connection handling and improve disconnection logic

--- ws/agent_routes.py
+++ ws/agent_routes.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from services import agent_service
 from models.message import Message
 
@@ -7,10 +7,16 @@ agent_router = APIRouter()
 
 
 @agent_router.websocket("/agent")
-async def on_message(websocket: WebSocket):
+async def on_connect(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data: List[dict] = await websocket.receive_json()
-        messages = [Message(**item) for item in data]
-        response = await agent_service.ask(messages)
-        await websocket.send_json([response])
+    is_connected = True
+    while is_connected:
+        try:
+            data: List[dict] = await websocket.receive_json()
+            messages = [Message(**item) for item in data]
+            response = await agent_service.ask(messages)
+
+            await websocket.send_json([response])
+
+        except WebSocketDisconnect:
+            is_connected = False
